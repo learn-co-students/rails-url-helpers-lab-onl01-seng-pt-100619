df766b458e2344cc1ada466501b4c7a45fc8613e
laffitte.digital@gmail.com <laffitte.digital@gmail.com>
2020-02-29 15:00:00 -0500

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,14 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+  
+  def status
+    if active
+      "This student is currently active."
+    else
+      "This student is currently inactive."
+    end
+  end
+
+
 end
